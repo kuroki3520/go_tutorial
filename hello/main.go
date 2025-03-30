139ffeaae6d1e06f13318c8f9a482e270f045561
kuroki3520 <kurokisyota520@gmail.com>
2025-03-30 23:26:48 +0900

    Complete third step of Go tutorial: Return and handle an error

--- hello/main.go
+++ hello/main.go
@@ -15,7 +15,12 @@ func main() {
     log.SetFlags(0)
 
     // Request a greeting message.
-    message := greetings.Hello("Gladys")
+    message, err := greetings.Hello("Gladys")
+    // If an error was returned, print it to the console and
+    // exit the program.
+    if err != nil {
+        log.Fatal(err)
+    }
 
     // If no error was returned, print the returned message
     // to the console.
