60cb217c00f236c8c8fb77827217010c4b4b2b4c
kuroki3520 <kurokisyota520@gmail.com>
2025-03-30 23:48:36 +0900

    Complete fourth step of Go tutorial: Return a random greeting

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,7 +3,14 @@ package greetings
 import (
     "errors"
     "fmt"
+    "math/rand"
+    "time"
 )
+
+// init sets initial values for variables used in the function.
+func init() {
+    rand.Seed(time.Now().UnixNano())
+}
 
 // Hello returns a greeting for the named person.
 func Hello(name string) (string, error) {
@@ -12,8 +19,14 @@ func Hello(name string) (string, error) {
         return "", errors.New("empty name")
     }
 
-    // If a name was received, return a value that embeds the name
-    // in a greeting message.
-    message := fmt.Sprintf("Hi, %v. Welcome!", name)
+    // Create a slice of message formats.
+    formats := []string{
+        "Hi, %v. Welcome!",
+        "Great to see you, %v!",
+        "Hail, %v! Well met!",
+    }
+
+    // Return a greeting that embeds the name in a message.
+    message := fmt.Sprintf(formats[rand.Intn(len(formats))], name)
     return message, nil
 } 
